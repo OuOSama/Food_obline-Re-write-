12ac8915a0e1b99256be525d12897b7ccb9aecfc
OuOSama <ouosamacute@gmail.com>
2023-11-05 15:34:59 +0700

    fix can't see status

--- admin/manage_cate.php
+++ admin/manage_cate.php
@@ -3,40 +3,44 @@
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Manage Catetagory</title>
+    <title>Manage Category</title>
     <?php include('../asset/head/head-admin.php');?>
 </head>
 <body>
     <div class="content">
-        <div class="container shadow p-5 mb-5 bg-body rounded">
-            <h1 align="center" class="p-1">Manage Category</h1>
+        <div class="container shadow mb-5 p-5 rounded">
+            <h1 align="center" class="p-1">Manage category</h1>
             <table class="table">
+                <form action="config_cate.php" method="post">
                     <thead align="center">
-                        <th>Cate Id</th>
-                        <th>Cate Name</th>
+                        <th>Cate ID</th>
+                        <th>Cate name</th>
                         <th colspan="2">Action</th>
                     </thead>
                     <tbody>
                         <form action="config_cate.php" method="post">
-                            <input                  type="hidden" name="cate_id"   class="form-control">
-                            <td><input              type="text"   name="cate_id"   class="form-control" placeholder="Cate_id"   disable></td>
-                            <td><input              type="text"   name="cate_name" class="form-control" placeholder="Cate_name" disable></td>
-                            <td colspan="2"><button type="submit" name="insert"    class="btn btn-secondary form-control">Insert</button></td>
+                            <input type="hidden" name="cate_id">
+                            <td><input type="text"     name="cate_id"      class="form-control"  placeholder="Cate Id"></td>
+                            <td><input type="password" name="cate_name"    class="form-control"  placeholder="Cate Name"></td>
+                            <td colspan="2"><button type="submit" name="insert" class="btn btn-secondary form-control">Insert</button></td>
                         </form>
                     </tbody>
+                    <!--PHP Zone-->
                     <?php 
                         include('../connect.php');
                         $sql = "SELECT * FROM cate_res";
-                        $query = mysqli_query($connect,$sql);
-                        while($result = mysqli_fetch_array($query)){ 
+                        $query = mysqli_query($connect, $sql);
+                    ?>
+                    <?php 
+                        while($result = mysqli_fetch_array($query)) {
                     ?>
                     <tbody>
-                        <form action="config_cate.php"method="post">
-                            <input type="hidden"      name="cate_id"           class="form-control" value="<?php echo$result['cate_id'];?>">
-                            <td><input type="text"    name="cate_id" disabled  class="form-control" value="<?php echo$result['cate_id'];?>"></td>
-                            <td><input type="text"    name="cate_name"         class="form-control" value="<?php echo$result['cate_name'];?>"></td>
-                            <td><button type="submit" name="update"            class="form-control btn btn-success">Update</button></td>
-                            <td><button type="submit" name="delete"            class="form-control btn btn-danger"> Delete</button></td>
+                        <form action="config_cate.php" method="post">
+                            <td><input  type="text"   name="cate_id"  disabled  class="form-control" value=<?php echo$result['cate_id'];?>></td>
+                            <input      type="hidden" name="cate_id"            class="form-control" value=<?php echo$result['cate_id'];?>>
+                            <td><input  type="text"   name="cate_name"          class="form-control" value=<?php echo$result['cate_name'];?>></td>
+                            <td><button type="submit" name="update"             class="form-control btn btn-success">Update</button></td>
+                            <td><button type="submit" name="delete"             class="form-control btn btn-danger" >Delete</button></td>
                         </form>
                     </tbody>
                     <?php }?>
--- admin/manage_member.php
+++ admin/manage_member.php
@@ -8,9 +8,10 @@
 </head>
 <body>
     <div class="content">
-        <div class="container shadow p-5 mb-5 bg-body rounded">
+        <div class="container shadow mb-5 p-5 rounded">
             <h1 align="center" class="p-1">Manage Member</h1>
             <table class="table">
+                <form action="config.php" method="post">
                     <thead align="center">
                         <th>User</th>
                         <th>Password</th>
@@ -20,58 +21,59 @@
                         <th>Phone</th>
                         <th>Address</th>
                         <th>Status</th>
-                        <th>&nbsp;&nbsp;&nbsp;Enable&nbsp;&nbsp;&nbsp;</th>
+                        <th>Enable</th>
                         <th colspan="2">Action</th>
                     </thead>
                     <tbody>
                         <form action="config.php" method="post">
                             <input type="hidden" name="mem_id">
-                            <td><input type="text"  name="mem_user"    class="form-control"></td>
-                            <td><input type="text"  name="mem_pass"    class="form-control"></td>
-                            <td><input type="text"  name="mem_name"    class="form-control"></td>
-                            <td><input type="text"  name="mem_last"    class="form-control"></td>
-                            <td><input type="email" name="mem_mail"    class="form-control"></td>
-                            <td><input type="tel"   name="mem_phone"   class="form-control"></td>
-                            <td><input type="text"  name="mem_address" class="form-control"></td>
+                            <td><input type="text"     name="mem_user"    class="form-control"></td>
+                            <td><input type="password" name="mem_pass"    class="form-control"></td>
+                            <td><input type="text"     name="mem_name"    class="form-control"></td>
+                            <td><input type="text"     name="mem_last"    class="form-control"></td>
+                            <td><input type="email"    name="mem_mail"    class="form-control"></td>
+                            <td><input type="tel"      name="mem_phone"   class="form-control"></td>
+                            <td><input type="text"     name="mem_address" class="form-control"></td>
                             <td>
                                 <select name="mem_status" class="form-select">
                                     <option value="user">User</option>
-                                    <option value="rest">Restaurant</option>
                                     <option value="rider">Rider</option>
+                                    <option value="rest">Restaurant</option>
                             </td>
                             <td>
-                                <select class="form-select" name="mem_check">
+                                <select name="mem_check" class="form-select">
                                     <option value="enable">Enable</option>
                                     <option value="disable">Disable</option>
-                                </select>
-                             </td>
+                            </td>
                             <td colspan="2"><button type="submit" name="insert" class="btn btn-secondary form-control">Insert</button></td>
                         </form>
                     </tbody>
-                    <?php $sql = "SELECT * FROM member WHERE mem_check = 'enable' ";?>
+                    <!--PHP Zone-->
+                    <?php $sql = "SELECT * FROM member";?>
                     <?php $query = mysqli_query($connect, $sql);
                           while($result = mysqli_fetch_array($query)) {
                     ?>
                     <tbody>
-                        <form action="config.php"   method="post">
-                            <input type="hidden"            name="mem_id"    value= <?php echo$result['mem_id'];?>>
-                            <td><input type="text"          name="mem_user"     class="form-control" value=<?php echo$result['mem_user'];?>></td>
-                            <td><input type="password"      name="mem_user"     class="form-control" value=<?php echo$result['mem_user'];?>></td>
-                            <td><input type="text"          name="mem_name"     class="form-control" value=<?php echo$result['mem_name'];?>></td>
-                            <td><input type="text"          name="mem_last"     class="form-control" value=<?php echo$result['mem_last'];?>></td>
-                            <td><input type="email"         name="mem_mail"     class="form-control" value=<?php echo$result['mem_mail'];?>></td>
-                            <td><input type="tel"           name="mem_phone"    class="form-control" value=<?php echo$result['mem_phone'];?>></td>
-                            <td><input type="text"          name="mem_address"  class="form-control" value=<?php echo$result['mem_address'];?>></td>
-                            <td><input type="text" disabled name="mem_status"   class="form-control" Value=<?php echo$result['mem_status'];?>></td>
-                            <input     type="hidden"        name="mem_status"   class="form-control" value=<?php echo$result['mem_status']; ?>>
+                        <form action="config.php" method="post">
+                            <input type="hidden"      name="mem_id" value="<?php echo$result['mem_id'];?>">
+                            <td><input  type="text"   name="mem_user"    class="form-control" value=<?php echo$result['mem_user'];?>></td>
+                            <td><input  type="text"   name="mem_pass"    class="form-control" value=<?php echo$result['mem_pass'];?>></td>
+                            <td><input  type="text"   name="mem_name"    class="form-control" value=<?php echo$result['mem_name'];?>></td>
+                            <td><input  type="text"   name="mem_last"    class="form-control" value=<?php echo$result['mem_last'];?>></td>
+                            <td><input  type="email"  name="mem_mail"    class="form-control" value=<?php echo$result['mem_mail'];?>></td>
+                            <td><input  type="tel"    name="mem_phone"   class="form-control" value=<?php echo$result['mem_phone'];?>></td>
+                            <td><input  type="text"   name="mem_address" class="form-control" value=<?php echo$result['mem_address'];?>></td>
+                            <td><input  type="text"   name="mem_status"  class="form-control" value=<?php echo$result['mem_status'];?> disabled></td>
+                            <input      type="hidden" name="mem_status"  class="form-control" value=<?php echo$result['mem_status'];?>>
                             <td>
                                 <select name="mem_check" class="form-select">
+                                    <option value="<?php echo$result['mem_check'];?>"><?php echo$result['mem_check'];?></option>
                                     <option value="enable"> Enable</option>
                                     <option value="disable">Disable</option>
                                 </select>
                             </td>
-                            <td><button type="submit" name="update" class="form-control btn btn-success">Update</button></td>
-                            <td><button type="submit" name="delete" class="form-control btn btn-danger"> Delete</button></td>
+                            <td><button type="submit" name="update"      class="form-control btn btn-success">Update</button></td>
+                            <td><button type="submit" name="delete"      class="form-control btn btn-danger" >Delete</button></td>
                         </form>
                     </tbody>
                     <?php }?>
